4234695fcb06b9afc2f0dc7a586f052495448799
Endurance Aloja <alojaeddie@gmail.com>
2024-01-20 20:59:31 +0000

    Add animation to navbar

--- src/components/LandingHeader.tsx
+++ src/components/LandingHeader.tsx
@@ -1,8 +1,13 @@
+import LandingHeaderAnimation from "./LandingHeaderAnimation";
 function LandingHeader() {
+  LandingHeaderAnimation();
   return (
-    <header className=" w-screen px-10 py-3 flex z-40 flex-row gap-28 absolute text-center justify-center">
+    <header
+      id="landing-header"
+      className=" w-screen px-10 py-3 flex z-40 flex-row gap-28 absolute text-center justify-center"
+    >
       <div className="flex items-center text-lg font-hero">
-        <a href="/">PokeLo</a>
+        <a href="/">PokeLoo</a>
       </div>
       <nav className="flex  justify-center">
         <ul className=" flex flex-row justify-between text-lg [&>li>a]:transition-colors [&>li>a]:duration-500 [&>li>a]:font-medium [&>li>a]:px-2 [&>li>a]:inline-block [&>li>a]:py-2">
@@ -16,7 +21,7 @@ function LandingHeader() {
             <a href="#">Contact</a>
           </li>
           <li>
-            <a href="#">PokeApi</a>
+            <a href="https://pokeapi.co/">PokeApi</a>
           </li>
         </ul>
       </nav>
@@ -28,6 +33,17 @@ function LandingHeader() {
           </li>
         </ul>
       </nav>
+      <div
+        id="menu-backdrop"
+        className={`
+          absolute bg-black/5 backdrop-blur-lg rounded
+          translate-x-[var(--left)] translate-y-[var(--top)]
+          left-0 top-0
+          w-[var(--width)] h-[var(--height)]
+          transition-all duration-500
+          ease-in-out opacity-0 -z-10
+        `}
+      ></div>
     </header>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
     fontFamily: {
       hero: ["Pokemon Solid", "Helvetica"],
     },
+    extend: {
+      animation: {
+        fade: "fadeOut 5s ease-in-out",
+      },
+
+      // that is actual animation
+      keyframes: (theme) => ({
+        fadeOut: {
+          "0%": { opacity: theme("colors.red.300") },
+          "100%": { backgroundColor: theme("colors.transparent") },
+        },
+      }),
+    },
   },
   content: ["./src/**/*.tsx", "./src/**/*.css"],
   plugins: [require("@tailwindcss/forms")],
