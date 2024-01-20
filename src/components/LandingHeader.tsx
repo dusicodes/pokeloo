d74a3d828a9f7a606b402ee9ea4ad7ab82bc532d
Endurance Aloja <alojaeddie@gmail.com>
2024-01-20 20:11:24 +0000

    Test Commit

--- src/components/LandingHeader.tsx
+++ src/components/LandingHeader.tsx
@@ -2,7 +2,7 @@ function LandingHeader() {
   return (
     <header className=" w-screen px-10 py-3 flex z-40 flex-row gap-28 absolute text-center justify-center">
       <div className="flex items-center text-lg font-hero">
-        <a href="/">PokeLoo</a>
+        <a href="/">PokeLo</a>
       </div>
       <nav className="flex  justify-center">
         <ul className=" flex flex-row justify-between text-lg [&>li>a]:transition-colors [&>li>a]:duration-500 [&>li>a]:font-medium [&>li>a]:px-2 [&>li>a]:inline-block [&>li>a]:py-2">
