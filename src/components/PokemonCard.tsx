55d1a80e2fcd48740e5cdb1c22aa429724e8eea6
Endurance Aloja <alojaeddie@gmail.com>
2024-01-22 15:50:32 +0000

    set up api for pokemon searching

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,20 +1,21 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-function PokemonCard() {
-  const [pokemon, setPokemon] = useState([]);
-  const URL = "https://pokeapi.co/api/v2/pokemon/ditto";
+import { useState } from "react";
 
-  useEffect(() => {
-    axios
-      .get(URL)
-      .then((response) => {
-        setPokemon(response.data);
-        console.log(response.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+function PokemonCard() {
+  // const [pokemon, setPokemon] = useState([]);
+  const [userInput, setUserInput] = useState<string>("charmander");
+  const [pokemonName, setPokemonName] = useState<string>("");
+  const [pokemonImage, setPokemonImage] = useState<string>("");
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/` + userInput)
+    .then((response) => response.json())
+    .then((data) => {
+      const { name, sprites } = data;
+      const { front_default } = sprites;
+
+      setPokemonName(name);
+      setPokemonImage(front_default);
+    })
+    .catch((error) => console.error(error));
 
   return (
     <>
@@ -23,9 +24,17 @@ function PokemonCard() {
           className="rounded-xl"
           placeholder="Enter a Pokemon"
           type="text"
+          name="pokemonInput"
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
         />
       </div>
-      <article className="h-[600px] w-[900px] bg-white rounded-3xl flex flex-row justify-between"></article>
+      <article className="h-[600px] w-[900px] bg-white rounded-3xl flex flex-row">
+        <img className="h-[512px] w-[512px]" src={pokemonImage}></img>
+        <div className="pt-6">
+          <h1 className=" font-bold text-4xl capitalize">{pokemonName}</h1>
+        </div>
+      </article>
     </>
   );
 }
