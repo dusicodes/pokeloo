deb641c9968b51cabbeb0a071731167b70f32e58
Endurance Aloja <alojaeddie@gmail.com>
2024-03-26 16:01:01 +0000

    started implementing stats for pokemon outputs

--- src/components/PokemonFind.tsx
+++ src/components/PokemonFind.tsx
@@ -1,37 +1,54 @@
 import { Button } from "@chakra-ui/react";
 import { Spinner } from "@chakra-ui/react";
 import { useState } from "react";
+import PokemonOutputInfo from "./PokemonOutputInfo";
 
 function PokemonFind() {
+  interface Pokemon {
+    name: string;
+  }
+
+  interface PokemonStat {
+    base_stat: number;
+  }
   // const [pokemon, setPokemon] = useState([]);
   const [userInput, setUserInput] = useState<string>("");
   // const [userClickInput, setUserClickInput] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [pokemonName, setPokemonName] = useState<string>("");
   const [pokemonImage, setPokemonImage] = useState<string>("");
+  const [pokemonName, setPokemonName] = useState<string>("");
+  const [pokemonSpeed, setPokemonSpeed] = useState<string>("");
+  const [pokemonDefense, setPokemonDefense] = useState<string>("");
+  const [pokemonAttack, sePokemonAttack] = useState<string>("");
+  const [pokemonHp, sePokemonHp] = useState<string>("");
+
   const [searchResults, setSearchResults] = useState<[]>([]);
 
-  const handleSubmit = (e: React.FormEvent) => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/` + userInput)
+  const outputPokemonData = (pokemonInput: string) => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/` + pokemonInput)
       .then((response) => response.json())
       .then((data) => {
-        const { name, sprites } = data;
+        const { name, sprites, stats } = data;
+
+        stats.map((stat: PokemonStat) => {
+          console.log(stat.base_stat);
+          if (stat.stat.name == "hp") {
+            console.log(stat.base_stat);
+          }
+        });
         const { front_default } = sprites;
-        setIsLoading(true);
         setPokemonName(name);
         setPokemonImage(front_default);
         setIsLoading(false);
       })
       .catch((error) => console.error(error));
-    e.preventDefault();
   };
-
   fetch(`https://pokeapi.co/api/v2/pokemon?limit=1000000&offset=0`)
     .then((response) => response.json())
     .then((data) => {
       const { results } = data;
-      console.log(results);
-      const resultsData = results.filter((pokemon) => {
+
+      const resultsData = results.filter((pokemon: Pokemon) => {
         return (
           userInput &&
           pokemon &&
@@ -43,10 +60,15 @@ function PokemonFind() {
     })
     .catch((error) => console.error(error));
 
-  // const handleResultClick = (value: string) => {
-  //   setUserInput(value);
-  //   // setUserClickInput(value);
-  // };
+  const handleSubmit = (e: React.FormEvent) => {
+    outputPokemonData(userInput);
+    e.preventDefault();
+  };
+
+  const handleResultClick = (value: string) => {
+    setUserInput(value);
+    outputPokemonData(value);
+  };
 
   return (
     <section className="flex flex-row gap-10 ">
@@ -60,13 +82,11 @@ function PokemonFind() {
         >
           Waiting for pokemon request... {isLoading && <Spinner />}
         </div>
-        <div className={isLoading === true ? "hidden" : "flex h-auto w-full"}>
-          <div className="">
-            <img className="h-[512px] w-[450px]" src={pokemonImage}></img>
-          </div>
-          <div className="pt-6 w-[50%] text-center">
-            <h1 className=" font-bold text-4xl capitalize">{pokemonName}</h1>
-          </div>
+        <div className={isLoading === true ? "hidden" : "flex h-auto w-full "}>
+          <PokemonOutputInfo
+            pokemonImage={pokemonImage}
+            pokemonName={pokemonName}
+          />
         </div>
       </article>
       <article className="flex flex-col h-[600px] w-auto p-4 bg-white rounded-xl gap-6">
@@ -88,7 +108,11 @@ function PokemonFind() {
         <div className=" flex flex-col gap-2 h-auto scroll overflow-y-scroll">
           {searchResults.map((results, idx) => {
             return (
-              <div className="rounded-xl hover:bg-slate-100 p-4" key={idx}>
+              <div
+                className="rounded-xl hover:bg-slate-100 cursor-pointer p-4"
+                key={idx}
+                onClick={() => handleResultClick(results.name)}
+              >
                 {results.name}
               </div>
             );
